5a6af7f33b937f41076c9cd6c492f35c6df2e5a5
HughEvans01 <testuser@example.com>
2019-02-18 14:40:11 +0000

    Completed extra challenge 1

---
+++ ExtraChallenge1.rb
@@ -0,0 +1,13 @@
+# ExtraChallenge1
+# Method will tell you if number is odd or even
+
+def oddOrEven(n)
+  if n.odd?
+    puts "#{n} is odd"
+  else
+    puts "#{n} is even"
+  end
+end
+
+oddOrEven(1)
+oddOrEven(2)
