9761db1f8e9de0b91f5fdc05a6b023f0c17dbe3a
HughEvans01 <testuser@example.com>
2019-02-18 13:50:59 +0000

    Completed challenge 3

---
+++ Challenge3.rb
@@ -0,0 +1,14 @@
+# Challenge3
+# Bank balance
+
+Bank_balance = 100
+
+def dinner_plans
+  if Bank_balance > 100
+    puts "You can go out for dinner"
+  else
+    puts "Stay at home"
+  end
+end
+
+dinner_plans()
